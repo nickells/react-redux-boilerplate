a5eee30933a3f3682a873fcc215935ac99ba97b7
nickells <ellsworth.nick@gmail.com>
2018-02-18 16:35:18 -0500

    made one reducer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,36 @@
 import 'babel-polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux'
+import { Route } from 'react-router-dom'
+import store, { history } from './Store'
+
+const Nothing = () => (
+  <div>
+    <h1>Hello, world.</h1>
+  </div>
+)
+
+class Root extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <React.Fragment>
+          <ConnectedRouter history={history}>
+            <React.Fragment>
+              <Route exact path="/" component={Nothing} />
+            </React.Fragment>
+          </ConnectedRouter>
+        </React.Fragment>
+      </Provider>
+    )
+  }
+}
 
 ReactDOM.render(
   <h1>Hello, world!</h1>,
